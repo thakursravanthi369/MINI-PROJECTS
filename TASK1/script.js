6062219612805362e04fd5b428aaba3b70009043
THAKUR SRAVANTHI <thakursravanthi1111@outlook.com>
2025-02-28 14:05:15 +0530

    Add files via upload

---
+++ TASK1/script.js
@@ -0,0 +1,8 @@
+document.getElementById('bookTrial').addEventListener('click', function() {
+    alert('Book Free Trial button clicked!');
+    //Add functionality for the button here.
+});
+
+document.getElementById("applyBtn").addEventListener("click", function() {
+    alert("Thank you for your interest! We will get in touch with you soon.");
+});
